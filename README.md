f38d8062542340cd17cb6813aaacdf05028cfa9a
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-09-24 13:07:07 +0300

    feat: install other modules and preparing to create a server using express and mongoose

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 npm run start
 
 # Example post query:
+```
 curl http://localhost:4000/add-todo \
   -X POST \
   -H "Content-Type: application/json" \
   -d '{"name":"Post 1", "content":"Lorem ipsum"}'
+```
 
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer().listen(8000);
-
-server.on('request', function(request:Request, response:Response) {
-  console.log(request.method);
-});
-console.log('server listening on 8000');
-
